5440340d75707a02fb9d6a09fcee932ac1225d89
Andrey Andruschyshyn <p0lir0id@yandex.ru>
2020-01-26 23:27:52 +0200

    auto-destroy fix for battle UI

--- python/gui/wgfm/views/battleView.py
+++ python/gui/wgfm/views/battleView.py
@@ -1,5 +1,6 @@
 ﻿
 from gui.Scaleform.framework.entities.BaseDAAPIComponent import BaseDAAPIComponent
+from gui.Scaleform.framework.entities.DisposableEntity import EntityState
 
 from gui.wgfm.events import g_eventsManager
 
@@ -11,14 +12,21 @@ class WGFMBattleViewMeta(BaseDAAPIComponent):
 		if self._isDAAPIInited():
 			self.flashObject.as_showMessage(text, color, lifeTime)
 
+	def destroy(self):
+		if self.getState() != EntityState.CREATED:
+			return
+		super(WGFMBattleViewMeta, self).destroy()
+
 class WGFMBattleView(WGFMBattleViewMeta):
 
 	def _populate(self):
 		super(WGFMBattleView, self)._populate()
 		g_eventsManager.showBattleMessage += self.__showBattleMessage
+		g_eventsManager.onDestroyBattle += self.destroy
 
 	def _dispose(self):
 		g_eventsManager.showBattleMessage -= self.__showBattleMessage
+		g_eventsManager.onDestroyBattle -= self.destroy
 		super(WGFMBattleView, self)._dispose()
 
 	def __showBattleMessage(self, message, color, lifetime):
