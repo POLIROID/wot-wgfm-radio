4e6a898824a95ebf259105dad69461f3163174a4
Andrey Andruschyshyn <poliroid@pm.me>
2022-08-07 21:17:24 +0300

    credentials

--- python/gui/netradio/__init__.py
+++ python/gui/netradio/__init__.py
@@ -1,11 +1,11 @@
 ﻿
-__author__ = "Andruschyshyn Andrey"
-__copyright__ = "Copyright 2021, poliroid"
-__credits__ = ["Andruschyshyn Andrey"]
+__author__ = "Andrii Andrushchyshyn"
+__copyright__ = "Copyright 2022, poliroid"
+__credits__ = ["Andrii Andrushchyshyn"]
 __license__ = "LGPL-3.0-or-later"
 __version__ = "1.0.2"
-__maintainer__ = "Andruschyshyn Andrey"
-__email__ = "poliroid@pm.me"
+__maintainer__ = "Andrii Andrushchyshyn"
+__email__ = "contact@poliroid.me"
 __status__ = "Production"
 
 from gui.netradio.data import *
